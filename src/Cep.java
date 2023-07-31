c5b0c9a1cfe2a4e9bdae0dcafcb0f92e76eb7fec
Vinícius Olímpio <viniolimpio3@gmail.com>
2023-07-31 18:28:09 -0300

    Commit inicial - busca ao viaCEP + salvando lista de endereços em arquivo

---
+++ src/Cep.java
@@ -0,0 +1,57 @@
+import com.google.gson.annotations.SerializedName;
+
+public class Cep {
+
+    public Cep(String cep, String logradouro, String complemento, String bairro, String cidade, String uf) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    @SerializedName("localidade")
+    private String cidade;
+    private String uf;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", uf='" + uf + '\'' +
+                ']';
+    }
+}
